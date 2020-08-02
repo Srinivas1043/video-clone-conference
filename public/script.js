af3c88879118aafce5f0b0ed51c9f91c9c2f45a4
srinivas1043 <sm1043@gmail.com>
2020-08-02 19:38:30 +0530

    small change in peer host blah blaj

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '/',
+    host: 'peerjs-server.herokuapp.com',
     secure: true,
-
-    port: '443',
+    port: 443
 });
 const myVideo = document.createElement('video')
 myVideo.muted = true
