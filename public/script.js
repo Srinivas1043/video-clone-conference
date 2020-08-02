cab41040a7024a4d800e889ef339fe364983a611
srinivas1043 <sm1043@gmail.com>
2020-08-02 17:58:29 +0530

    Changed port in peer js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: 'process.env.PORT || 3001'
+    host: 'adi-dhool.herokuapp.com',
+    secure: true,
+    port: 443
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
