28eda3b4e450f7801df208b988e688f0645a2f74
srinivas1043 <sm1043@gmail.com>
2020-08-02 16:50:22 +0530

    change port link

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
+var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -25,5 +26,4 @@ io.on('connection', socket => {
         })
     })
 })
-
-server.listen(3000)
+server.listen(port)
